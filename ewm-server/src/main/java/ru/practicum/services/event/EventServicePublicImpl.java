dfc98c5999d1109034312679b3e6003e6f69c257
Bolohonov <bolohonov@list.ru>
2022-09-22 07:46:52 +0400

    fix: some classes

--- ewm-server/src/main/java/ru/practicum/services/event/EventServicePublicImpl.java
+++ ewm-server/src/main/java/ru/practicum/services/event/EventServicePublicImpl.java
@@ -75,9 +75,8 @@ public class EventServicePublicImpl implements EventServicePublic {
 
     private String getSortString(String sort) {
         String result = "id";
-        if (sort != null && sort.equals("EVENT_DATE")) {
+        if (sort != null && (sort.equals("EVENT_DATE") || sort.trim().equals("EVENTDATE"))) {
             result = "eventDate";
-
         }
         if (sort != null && sort.equals("VIEWS")) {
             result = "views";
